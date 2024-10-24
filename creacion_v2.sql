751a015eb122cfc52e248c73a9bf161e9374ee98
octavaliente <ovaliente@frba.utn.edu.ar>
2024-10-24 00:29:19 -0300

    Cambios en localidad, prov  y domicilio. Agrego almacen

--- creacion_v2.sql
+++ creacion_v2.sql
@@ -287,11 +287,26 @@ FROM gd_esquema.Maestra M
 WHERE M.CLI_USUARIO_DOMICILIO_PROVINCIA IS NOT NULL
 
 INSERT INTO jafo.localidad (nombre, provincia_codigo)
+(
 SELECT DISTINCT M.CLI_USUARIO_DOMICILIO_LOCALIDAD, jafo.provincia.codigo 
 FROM gd_esquema.Maestra M
 INNER JOIN jafo.provincia ON M.CLI_USUARIO_DOMICILIO_PROVINCIA = jafo.provincia.nombre
 
+UNION
+
+SELECT DISTINCT M.VEN_USUARIO_DOMICILIO_LOCALIDAD, jafo.provincia.codigo 
+FROM gd_esquema.Maestra M
+INNER JOIN jafo.provincia ON M.VEN_USUARIO_DOMICILIO_PROVINCIA = jafo.provincia.nombre
+
+UNION 
+
+SELECT DISTINCT M.ALMACEN_Localidad, jafo.provincia.codigo 
+FROM gd_esquema.Maestra M
+INNER JOIN jafo.provincia ON M.ALMACEN_PROVINCIA = jafo.provincia.nombre
+)
+
 INSERT INTO jafo.domicilio (calle, numero_calle, piso, depto, cp ,localidad_codigo)
+(
 SELECT M.CLI_USUARIO_DOMICILIO_CALLE,
 	   M.CLI_USUARIO_DOMICILIO_NRO_CALLE,
 	   M.CLI_USUARIO_DOMICILIO_PISO,
@@ -305,7 +320,48 @@ where not exists (
 	where calle = M.CLI_USUARIO_DOMICILIO_CALLE 
 	and numero_calle = M.CLI_USUARIO_DOMICILIO_NRO_CALLE
 	and cp = M.CLI_USUARIO_DOMICILIO_CP
+	)
+UNION
+
+SELECT M.VEN_USUARIO_DOMICILIO_CALLE,
+	   M.VEN_USUARIO_DOMICILIO_NRO_CALLE,
+	   M.VEN_USUARIO_DOMICILIO_PISO,
+	   M.VEN_USUARIO_DOMICILIO_DEPTO,
+	   M.VEN_USUARIO_DOMICILIO_CP,
+	   jafo.localidad.codigo
+FROM gd_esquema.Maestra M
+INNER JOIN jafo.localidad ON M.VEN_USUARIO_DOMICILIO_LOCALIDAD = jafo.localidad.nombre
+where not exists (
+	select 1 from jafo.domicilio
+	where calle = M.VEN_USUARIO_DOMICILIO_CALLE 
+	and numero_calle = M.VEN_USUARIO_DOMICILIO_NRO_CALLE
+	and cp = M.VEN_USUARIO_DOMICILIO_CP
+	)
+UNION 
+
+SELECT M.ALMACEN_CALLE,
+	   M.ALMACEN_NRO_CALLE,
+	   null,
+	   null,
+	   null,
+	   jafo.localidad.codigo
+FROM gd_esquema.Maestra M
+INNER JOIN jafo.localidad ON M.ALMACEN_Localidad = jafo.localidad.nombre
+where not exists (
+	select 1 from jafo.domicilio
+	where calle = M.ALMACEN_CALLE 
+	and numero_calle = M.ALMACEN_NRO_CALLE
+	)
 )
+
+INSERT INTO jafo.almacen (codigo, domicilio_codigo, costo_dia_alquiler)
+SELECT DISTINCT M.ALMACEN_CODIGO, 
+			    jafo.domicilio.codigo,
+				M.ALMACEN_COSTO_DIA_AL
+FROM gd_esquema.Maestra M
+INNER JOIN jafo.domicilio 
+	ON calle = M.ALMACEN_CALLE 
+	   and numero_calle = M.ALMACEN_NRO_CALLE
 
 insert into jafo.rubro (descripcion)
     select distinct PRODUCTO_RUBRO_DESCRIPCION 
