528dc5106cb611fb4670ab93dd73fc748d2cff6e
octavaliente <ovaliente@frba.utn.edu.ar>
2024-11-22 21:24:29 -0300

    Vistas 3 4 y 5

--- bi/vistas_bi.sql
+++ bi/vistas_bi.sql
@@ -19,3 +19,78 @@ inner JOIN jafo.bi_dim_tiempo dt ON dt.id_tiempo = hp.tiempo_id
 GROUP BY dm.id_marca, dm.descripcion, dt.Anio;
 go
 
+--3 
+CREATE VIEW jafo.view_promedio_venta_mensual_por_provincia AS
+SELECT 
+    ub.provincia AS Provincia,
+    dt.anio AS Año,
+    dt.mes AS Mes,
+    AVG(hv.importe_total) AS VentaPromedioMensual
+FROM 
+    jafo.bi_hechos_ventas hv
+INNER JOIN 
+    jafo.bi_dim_ubicacion ub ON hv.idUbicacionAlmacen = ub.idUbicacion
+INNER JOIN 
+    jafo.bi_dim_tiempo dt ON hv.idTiempo = dt.id_tiempo
+GROUP BY 
+    ub.provincia, dt.anio, dt.mes;
+GO
+
+--4
+
+CREATE VIEW jafo.view_top_5_rubros_por_rendimiento AS
+SELECT 
+    dt.anio AS Año,
+    dt.cuatrimestre AS Cuatrimestre,
+    ub.localidad AS Localidad,
+    re.descripcion_rango AS RangoEtario,
+    rb.rubro AS Rubro,
+    SUM(hv.importe_total) AS TotalVentas,
+    RANK() OVER (
+        PARTITION BY dt.anio, dt.cuatrimestre, ub.localidad, re.idRangoEtario 
+        ORDER BY SUM(hv.importe_total) DESC
+    ) AS Ranking
+FROM 
+    jafo.bi_hechos_ventas hv
+INNER JOIN 
+    jafo.bi_dim_tiempo dt ON hv.idTiempo = dt.id_tiempo
+INNER JOIN 
+    jafo.bi_dim_ubicacion ub ON hv.idUbicacionCliente = ub.idUbicacion
+INNER JOIN 
+    jafo.bi_dim_rubro rb ON hv.idRubro = rb.idRubro
+INNER JOIN 
+    jafo.bi_dim_rango_etario re ON hv.idRangoEtario = re.idRangoEtario
+GROUP BY 
+    dt.anio, dt.cuatrimestre, ub.localidad, re.idRangoEtario, re.descripcion_rango, rb.rubro
+HAVING 
+    RANK() OVER (
+        PARTITION BY dt.anio, dt.cuatrimestre, ub.localidad, re.idRangoEtario 
+        ORDER BY SUM(hv.importe_total) DESC
+    ) <= 5;
+GO
+
+--5
+CREATE VIEW jafo.view_volumen_ventas_rango_horario AS
+SELECT 
+    dt.anio AS Año,
+    dt.mes AS Mes,
+    CASE 
+        WHEN dt.hora BETWEEN 0 AND 5 THEN 'Madrugada'
+        WHEN dt.hora BETWEEN 6 AND 11 THEN 'Mañana'
+        WHEN dt.hora BETWEEN 12 AND 17 THEN 'Tarde'
+        WHEN dt.hora BETWEEN 18 AND 23 THEN 'Noche'
+    END AS RangoHorario,
+    COUNT(hv.idVenta) AS CantidadVentas
+FROM 
+    jafo.bi_hechos_ventas hv
+INNER JOIN 
+    jafo.bi_dim_tiempo dt ON hv.idTiempo = dt.id_tiempo
+GROUP BY 
+    dt.anio, dt.mes, 
+    CASE 
+        WHEN dt.hora BETWEEN 0 AND 5 THEN 'Madrugada'
+        WHEN dt.hora BETWEEN 6 AND 11 THEN 'Mañana'
+        WHEN dt.hora BETWEEN 12 AND 17 THEN 'Tarde'
+        WHEN dt.hora BETWEEN 18 AND 23 THEN 'Noche'
+    END;
+GO
